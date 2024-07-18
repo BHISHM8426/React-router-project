e22a7419c0602895632a33019ee6dde2a696f50f
BHISHM8426 <bhishm8426@gmail.com>
2024-07-18 12:36:13 +0530

    first commit

---
+++ router/src/Components/Github/Github.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Github (){
+    return(
+        <>
+        <div className="flex flex-wrap justify-around">
+            <div> <img src="https://tse1.mm.bing.net/th?id=OIP.eTCbdR_AFzbqHMPXhrJWUQHaEK&pid=Api&P=0&h=180" alt="" /></div>
+            <div>
+            <h1 className="text-4xl text-black py-4">My Github Account</h1>
+            <a href="https://github.com/BHISHM8426"> Click here for Github</a>
+            </div>
+            
+        </div>
+        </>
+    )
+
+}
+
+
+export  default Github;
